51e309ce233e7a7c14ed77f6fcd88ec56f4bd7f0
anshika2711 <137252770+anshika2711@users.noreply.github.com>
2025-07-06 16:11:47 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="NayaStrongPassword"
+        password="NayaStrongPassword",
         database="pricing_db"
     )
 
